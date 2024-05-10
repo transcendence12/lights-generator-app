ffb50e59473e913189685db8fbf93626c85e2162
transcendence12 <gosiek.krawczuk@gmail.com>
2024-05-10 12:17:49 +0200

    Revert "feat: create counter"

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import { Counter } from "./components/Counter";
-import { Text } from "./components/Text";
 
 function App() {
-  return (
-    <div>
-      <nav className="nav">
-        <Text>0 bulbs</Text>
-        <Counter text={"rows"} count={0} />
-        <Counter text={"columns"} count={0} />
-      </nav>
-    </div>
-  );
+  return <div>Lights generator</div>;
 }
 
 export default App;
