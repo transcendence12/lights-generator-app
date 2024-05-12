a125be760d614ee4edf1b911584e191acbf9169d
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2024-05-12 10:23:02 +0200

    feat: changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,30 @@ import "./App.css";
 import { Counter } from "./components/Counter";
 import { Text } from "./components/Text";
 
+import { useState } from 'react';
+
 function App() {
+
+  const [value, setValue] = useState(0);
+
+  const handleIncrement = () => {
+    console.log("value increment", value)
+      setValue(value + 1);
+  };
+
+  const handleDecrement = () => {
+    console.log("value decrement", value)
+      if (value <= 0) {
+          return
+      }
+      setValue(value - 1)
+  }
   return (
     <div>
-      <nav className="nav">
+      <nav className={"nav"}>
         <Text>0 bulbs</Text>
-        <Counter text={"rows"} count={0} />
-        <Counter text={"columns"} count={0} />
+        <Counter text={"rows"} count={value} onDecrement={handleDecrement}/>
+        <Counter text={"columns"} count={value} onIncrement={handleIncrement}/>
       </nav>
     </div>
   );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,30 +3,18 @@ import { IconDecrement } from "./icons/IconDecrement";
 import { IconIncrement } from "./icons/IconIncrement";
 
 import styles from "./Counter.module.css";
-import { useState } from "react";
 
 export const Counter = (props) => {
-    const [value, setValue] = useState(0);
 
-    const handleIncrement = () => {
-        setValue(value + 1);
-    };
-
-    const handleDecrement = () => {
-        if (value <= 0) {
-            return
-        }
-        setValue(value - 1)
-    }
 
   return (
     <div className={styles.wrapper}>
       <Text>
-        {value} {props.text}
+        {props.count} {props.text}
       </Text>
       <nav>
-        <IconDecrement onClick={handleDecrement} />
-        <IconIncrement onClick={handleIncrement} />
+        <IconDecrement onClick={props.onDecrement} />
+        <IconIncrement onClick={props.onIncrement} />
       </nav>
     </div>
   );
